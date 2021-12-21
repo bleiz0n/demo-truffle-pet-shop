cfa59c9d87b3520fe50e69529f5ae78ed8bc5240
bleiz0n <alberto.rguez.belizon@gmail.com>
2021-12-21 11:25:29 +0100

    Demo finish

--- src/js/app.js
+++ src/js/app.js
@@ -24,17 +24,28 @@ App = {
   },
 
   initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+
+      try{
+        await window.ethereum.request({ method: "eth_requestAccounts"});
+      } catch {
+        console.log("User denied connection");
+      }
+    }
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON("Adoption.json", function(data) {
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact); // ABI
+
+      App.contracts.Adoption.setProvider(App.web3Provider); // ADDRESS
+
+      return App.markAdopted();
+    });
 
     return App.bindEvents();
   },
@@ -44,9 +55,21 @@ App = {
   },
 
   markAdopted: function() {
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    App.contracts.Adoption.deployed().then(function(instance) {
+      adoptionInstance = instance;
+      
+      return adoptionInstance.getAdopters.call();
+    }).then(function(adopters) {
+      for(var i = 0; i < adopters.length; i++){
+        if(adopters[i] !== '0x0000000000000000000000000000000000000000'){
+          $(`.btn-adopt[data-id=${i}]`).text("Adoptado").attr("disabled", true);
+        }
+      }
+    }).catch(function(error){
+      console.log(error);
+    });
   },
 
   handleAdopt: function(event) {
@@ -54,9 +77,23 @@ App = {
 
     var petId = parseInt($(event.target).data('id'));
 
-    /*
-     * Replace me...
-     */
+    web3.eth.getAccounts(function(error, accounts){
+      if(error){
+        console.log(error);
+        return;
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Adoption.deployed().then(function(instance){
+        return instance.adopt(petId, {from: account});
+
+      }).then(function(result){
+        return App.markAdopted();
+      }).catch(function(err){
+        console.log(err.message);
+      });
+    });
   }
 
 };
